4dfc37504c5201ae9a3cc4b0edd97323525c9f2d
proegssilb <proegssilb@gmail.com>
2023-11-25 12:36:08 -0500

    Convert repository to use a multi-year workspace as the sample code (implement #103) (#110)
    - Macros crate split into multiple files (possibly more to be done here)
    - Example usage code changed to multi-year workspace
    - Applied new usage to borrowed 2015 code
    - Deleted the old sample usage files
    - Tests and binaries now run
    - Each day has to be listed in the year's Cargo.toml as a binary. I don't like it either.
    - The year has to be specified in the aoc macro. I don't like it either.

---
+++ sample/2015/src/day2.rs
@@ -0,0 +1,74 @@
+use aoc_zen_runner_macros::{aoc, aoc_case, generator, solver};
+
+type Gift = (u32, u32, u32);
+
+#[aoc(2015, day02)]
+pub mod solutions {
+    use super::*;
+
+    #[generator(gen)]
+    pub fn input_generator(input: &str) -> Vec<Gift> {
+        input
+            .lines()
+            .map(|l| {
+                let mut gift = l.trim().split('x').map(|d| d.parse().unwrap());
+                (gift.next().unwrap(), gift.next().unwrap(), gift.next().unwrap())
+            })
+            .collect()
+    }
+
+    #[solver(part1, dotchain)]
+    pub fn solve_part1(input: Vec<Gift>) -> u32 {
+        input
+            .iter()
+            .map(|&(l, w, h)| {
+                let (s1, s2) = smallest_side((l, w, h));
+
+                2 * l * w + 2 * w * h + 2 * h * l + s1 * s2
+            })
+            .sum()
+    }
+
+    #[solver(part1, for_loop)]
+    pub fn solve_part1_for(input: Vec<Gift>) -> u32 {
+        let mut sum = 0;
+
+        for (l, w, h) in input {
+            let (s1, s2) = smallest_side((l, w, h));
+
+            sum += 2 * l * w + 2 * w * h + 2 * h * l + s1 * s2;
+        }
+
+        sum
+    }
+
+    #[solver(part2, dotchain)]
+    pub fn solve_part2(input: Vec<Gift>) -> u32 {
+        input
+            .iter()
+            .map(|&(l, w, h)| {
+                let (s1, s2) = smallest_side((l, w, h));
+
+                (s1 + s2) * 2 + l * w * h
+            })
+            .sum()
+    }
+
+    fn smallest_side((l, w, h): Gift) -> (u32, u32) {
+        let mut vec = vec![l, w, h];
+        vec.sort();
+
+        (vec[0], vec[1])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[aoc_case(58u32, 34u32)]
+    const input1: &str = "2x3x4";
+
+    #[aoc_case(43u32, 14u32)]
+    const input2: &str = "1x1x10";
+}
---
+++ sample/2022/src/lib.rs
@@ -0,0 +1,9 @@
+extern crate criterion;
+extern crate microbench;
+extern crate pprof;
+
+pub mod day1;
+pub mod day2;
+pub mod day3;
+pub mod day4;
+pub mod day5;
