b9e69bff6ef098cfed059e9866636f40115b725c
Restyled.io <commits@restyled.io>
2023-11-30 13:34:06 +0000

    Restyled by rustfmt

--- runner/src/templates/dayX.rs
+++ runner/src/templates/dayX.rs
@@ -1,37 +1,27 @@
-use aoc_zen_runner_macros::{aoc, solution, solver, generator, aoc_case};
+use aoc_zen_runner_macros::{aoc, aoc_case, generator, solution, solver};
 
 #[aoc({{year}}, {{day}})]
 pub mod solutions {
     use super::*;
 
     #[generator(gen)]
-    pub fn input_generator(input: &str) -> Vec<i32> {
-
-    }
+    pub fn input_generator(input: &str) -> Vec<i32> {}
 
     // ----------------------- Part 1 -----------------------
 
     #[solver(part1, draft_solvr)]
-    pub fn solve_part1(input: Vec<i32>) -> u32 {
-
-    }
+    pub fn solve_part1(input: Vec<i32>) -> u32 {}
 
     #[solution(part1, draft_soln)]
-    pub fn part1_draft(input: &str) -> u32 {
-
-    }
+    pub fn part1_draft(input: &str) -> u32 {}
 
     // ----------------------- Part 2 -----------------------
 
     #[solver(part2, draft_solvr)]
-    pub fn solve_part2(input: Vec<i32>) -> u32 {
-
-    }
+    pub fn solve_part2(input: Vec<i32>) -> u32 {}
 
     #[solution(part2, draft_soln)]
-    pub fn part2_draft(input: &str) -> u32 {
-
-    }
+    pub fn part2_draft(input: &str) -> u32 {}
 }
 
 #[cfg(test)]
